bb19081ee3226229af6ea7950877ccafea155f50
RaulPabianCruz <ito.pabian@gmail.com>
2024-09-18 12:48:39 -0700

    Update port value in app.js

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use((err, req, res, next) => {
     res.status(err.statusCode || 500).send(err.message);
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
